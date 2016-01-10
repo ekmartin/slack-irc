8ae95b47fd52235256b447adea623dcd6a859112
ekmartin <mail@ekmartin.com>
2016-01-10 22:15:28 +0100

    Update dependencies

--- test/fixtures/test-javascript-config.js
+++ test/fixtures/test-javascript-config.js
@@ -1,17 +1,17 @@
 module.exports = [
   {
-    'nickname': 'test',
-    'server': 'irc.bottest.org',
-    'token': 'testtoken',
-    'channelMapping': {
+    nickname: 'test',
+    server: 'irc.bottest.org',
+    token: 'testtoken',
+    channelMapping: {
       '#slack': '#irc'
     }
   },
   {
-    'nickname': 'test2',
-    'server': 'irc.bottest.org',
-    'token': 'testtoken',
-    'channelMapping': {
+    nickname: 'test2',
+    server: 'irc.bottest.org',
+    token: 'testtoken',
+    channelMapping: {
       '#slack': '#irc'
     }
   }
